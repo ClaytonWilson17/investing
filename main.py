54610d39cabea971b8acdc9d0422ca9f496b7513
ClaytonWilson12 <54342029+ClaytonWilson12@users.noreply.github.com>
2022-12-08 19:01:03 -0500

    update folder name

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # Main file which controls all of the inputs to the functions
 
-from lib import get_technical_indicators, output_to_csv, sell_signal, buy_signal
+from helper_functions import get_technical_indicators, output_to_csv, sell_signal, buy_signal
 
 # List of stocks to get technical indicator data on
 NASDAQ_symbols = ['CSX', 'AMD', 'GOOGL', 'AMZN', 'DBX', 'AAPL', 'SBUX', 'MSFT', 'CSCO', 'TSCO', 'NVDA']
@@ -9,7 +9,7 @@ NYSE_symbols = ['BAC', 'SYF', 'IBM', 'DKS', 'KR', 'UNH', 'DUK', 'TM', 'CNI', 'V'
 technical_data = []
 technical_data =get_technical_indicators.get_tech_indicators(NYSE_symbols=NYSE_symbols, NASDAQ_symbols=NASDAQ_symbols)
 
-
+buy_indicators = buy_signal.determine_buy_signals()
 
 output_to_csv.print_to_csv(technical_data, 'testing')
 
