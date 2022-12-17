160cec8cedcc0a80335da9083d4598919648033d
ClaytonWilson12 <54342029+ClaytonWilson12@users.noreply.github.com>
2022-12-16 20:29:06 -0500

    complete markus signal

--- helper_functions/markus_signal.py
+++ helper_functions/markus_signal.py
@@ -5,6 +5,10 @@ def based_on_RSI(RSI, past_RSI):
     if float(RSI) > float(50):
         if float(RSI) > float(past_RSI):   
             signal = 'buy'
+    
+    if float(RSI) < float(50):
+        if float(RSI) < float(past_RSI):   
+            signal = 'sell'
     return (signal)
 
 
@@ -13,6 +17,10 @@ def based_on_stochastic(stochastic, past_stochastic):
     if float(stochastic) > float(50):
         if float(stochastic) > float(past_stochastic):
             signal = 'buy'
+
+    if float(stochastic) < float(50):
+        if float(stochastic) < float(past_stochastic):
+            signal = 'sell'
     return (signal)
 
 
@@ -24,28 +32,30 @@ def based_on_macd(MACD_line, signal_line):
     MACD_line = float(MACD_line)
     signal_line = float(signal_line)
     signal = None
-
     if MACD_line > signal_line:
         signal = 'buy'
+    
+    if MACD_line < signal_line:
+        signal = 'sell'
 
     return (signal)
 
 
-def determine_signals(RSI=None, past_RSI=None, stochastic=None, past_stochastic=None, macd_line=None, macd_signal=None):
+def determine_buy_signals(RSI=None, past_RSI=None, stochastic=None, past_stochastic=None, macd_line=None, macd_signal=None):
 
     '''
     All values except price are set to None by default, it will test whatever values you input to see if there is a signal signal on one of the indicators
 
-    Returns ['buy']
+    Returns ['buy', [signals]]
     '''
     # These variables will be set to 'buy' if the technical indicators suggest to signal
-    RSI = False
-    macd = False
+    
+    
     signals_that_are_buy = []
     
     if RSI is not None:
-        RSI = based_on_RSI(RSI, past_RSI=past_RSI)
-        if RSI == 'buy':
+        RSI_signal = based_on_RSI(RSI, past_RSI=past_RSI)
+        if RSI_signal == 'buy':
             signals_that_are_buy.append("RSI")
 
     if stochastic is not None:
@@ -57,8 +67,43 @@ def determine_signals(RSI=None, past_RSI=None, stochastic=None, past_stochastic=
         macd = based_on_macd(macd_line, macd_signal)
         if macd == 'buy':
             signals_that_are_buy.append("MACD")
-
-    if RSI == 'buy' or macd == 'buy' or stoch == 'buy':
+    
+    if (RSI_signal == 'buy' and macd == 'buy' and stoch == 'buy'):
+        
         return (['buy', signals_that_are_buy])
+    else:
+        return (["No Signal", "None"])
+
+
+def determine_sell_signals(RSI=None, past_RSI=None, stochastic=None, past_stochastic=None, macd_line=None, macd_signal=None):
+    
+    '''
+    All values except price are set to None by default, it will test whatever values you input to see if there is a signal signal on one of the indicators
+
+    Returns ['sell', [signals]]
+    '''
+    # These variables will be set to 'buy' if the technical indicators suggest to signal
+    
+    macd = ''
+    stoch = ''
+    signals_that_are_sell = []
+    
+    if RSI is not None:
+        RSI = based_on_RSI(RSI, past_RSI=past_RSI)
+        if RSI == 'sell':
+            signals_that_are_sell.append("RSI")
+
+    if stochastic is not None:
+        stoch = based_on_stochastic(stochastic=stochastic, past_stochastic=past_stochastic)
+        if stoch == 'sell':
+            signals_that_are_sell.append("stochastic")
+
+    if macd_line is not None and macd_signal is not None:
+        macd = based_on_macd(macd_line, macd_signal)
+        if macd == 'sell':
+            signals_that_are_sell.append("MACD")
+
+    if (RSI == 'sell' and macd == 'sell' and stoch == 'sell'):
+        return (['sell', signals_that_are_sell])
     else:
         return (["No Signal", "None"])
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # Main file which controls all of the inputs to the functions
 
 from helper_functions import custom_signal, get_technical_indicators, sell_signal, general, send_email, get_fundamental_indicators, markus_signal
+import os
 
 # List of stocks to get technical indicator data on
 #NASDAQ_symbols = ['CSX', 'AMD', 'GOOGL', 'AMZN', 'DBX', 'AAPL', 'SBUX', 'MSFT', 'CSCO', 'TSCO', 'NVDA']
@@ -30,7 +31,7 @@ general.fileSaveCache(data_path, technical_data, datestamp=True)
 historical_indicators = general.fileLoadCache(data_path)
 
 
-
+'''
 
 # Find stocks based on custom signal
 stocks_to_buy = []
@@ -52,7 +53,7 @@ for stocks in custom_stocks_to_buy:
 custom_path = general.resultsPath('Custom Signal.csv')
 if len(custom_stocks_to_buy) > 0:
     general.listOfDictsToCSV(custom_stocks_to_buy, custom_path)
-
+'''
 
 
 
@@ -62,42 +63,65 @@ if len(custom_stocks_to_buy) > 0:
 
 # Find stocks based on Markus signal
 stocks_to_buy = []
+stocks_to_sell = []
 for data in technical_data:
     past_data = general.get_historical_indicators(sym=data['Symbol'], days_ago=1)
     if past_data != 'no data':
-        signal = markus_signal.determine_signals(RSI=data['RSI'], past_RSI=past_data['RSI'], stochastic=data['Stochastic'], past_stochastic=past_data['Stochastic'] ,macd_line=data['MACD_line'])
+        signal = []
+        signal = markus_signal.determine_buy_signals(RSI=data['RSI'], past_RSI=past_data['RSI'], stochastic=data['Stochastic'], past_stochastic=past_data['Stochastic'] ,macd_line=data['MACD_line'], macd_signal=data['MACD_signal'])
         if signal[0] == 'buy':
+            print (signal)
             data['Based on Indicators'] = signal[1]
             data['Signal'] = signal[0]
             stocks_to_buy.append(data)
-        else: 
-            print (str(data['Symbol']) + " did not pass the Markus signal")
-Markus_stocks_to_buy = general.clean_list_of_dicts(stocks_to_buy)
+        signal = markus_signal.determine_sell_signals(RSI=data['RSI'], past_RSI=past_data['RSI'], stochastic=data['Stochastic'], past_stochastic=past_data['Stochastic'] ,macd_line=data['MACD_line'], macd_signal=data['MACD_signal'])
+        if signal[0] == 'sell':
+            data['Based on Indicators'] = signal[1]
+            data['Signal'] = signal[0]
+            stocks_to_sell.append(data)
 
-for stocks in Markus_stocks_to_buy:
+
+markus_stocks_to_buy = general.clean_list_of_dicts(stocks_to_buy)
+markus_stocks_to_sell = general.clean_list_of_dicts(stocks_to_sell)
+
+# Add fundamental analysis data back to dictionary
+for stocks in markus_stocks_to_buy:
+    for fundamental_data in list_of_dicts:
+        if stocks['Symbol'] == fundamental_data['symbol']:
+            stocks.update(fundamental_data)
+for stocks in markus_stocks_to_sell:
     for fundamental_data in list_of_dicts:
         if stocks['Symbol'] == fundamental_data['symbol']:
             stocks.update(fundamental_data)
 
-Markus_path = general.resultsPath('Markus Signal.csv')
-if len(Markus_stocks_to_buy) > 0:
-    general.listOfDictsToCSV(Markus_stocks_to_buy, Markus_path)
+
+# Output results from markus indicator to csv and store the file path to email later
+files = []
+
+markus_buy_path = general.resultsPath('Markus Buy Signal.csv')
+if len(markus_stocks_to_buy) > 0:
+    files.append(markus_buy_path)
+    general.listOfDictsToCSV(markus_stocks_to_buy, markus_buy_path)
+markus_sell_path = general.resultsPath('Markus Sell Signal.csv')
+if len(markus_stocks_to_sell) > 0:
+    files.append(markus_sell_path)
+    general.listOfDictsToCSV(markus_stocks_to_sell, markus_sell_path)
 
 
 
-allpath = general.resultsPath('All Stocks.csv')
-general.listOfDictsToCSV(technical_data, allpath)
+#allpath = general.resultsPath('All Stocks.csv')
+#general.listOfDictsToCSV(technical_data, allpath)
 
 
 
 # Email out the files
-subject = "Stock data for the day"
-body = "This is an email containing stock data sent from the Python investing bot."
-receiver_email = ""
+general.get_env_vars()
+subject = "Stock signals for the day"
+body = "Hello humans, please see the attached csv files for the current buy and sell signals for the day...  buy (sell a put)  sell (sell a call)"
+receiver_emails = []
+receiver_emails.append(os.environ['simon_email'])
+receiver_emails.append(os.environ['clayton_email'])
 
-files = []
-files.append(custom_path)
-#files.append(allpath)
-
-#send_email.send_email(subject=subject, body=body, receiver_email=receiver_email, files=files)
+for reciever in receiver_emails:
+    send_email.send_email(subject=subject, body=body, receiver_email=reciever, files=files)
 
