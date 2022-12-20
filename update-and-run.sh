7b904d40a7fad9f1a7f6572233691ca68501c113
Simon Owens <sowens@vailsys.com>
2022-12-20 10:58:39 -0600

    simon's server is old and shitty. has to run in a docker container to avoid permissions, dependencies, and python issues

--- update-and-run.sh
+++ update-and-run.sh
@@ -2,5 +2,5 @@
 
 mkdir -p data
 git pull
-pip3 install -r requirements.txt
-python3 main.py > data/log.txt
+# inside python container
+docker container run -w="/src/" -v $(pwd):/src/ -t python:3.10 /bin/bash -c "pip3 install -r requirements.txt; python3 main.py > data/log.txt"
