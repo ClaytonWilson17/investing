be7b929505bdece163739dda0bb61dad3596bc18
Simon Owens <sowens@vailsys.com>
2023-02-03 10:23:43 -0600

    fixed stock data. composite symbols still don't work

--- helper_functions/get_fundamental_indicators_test.py
+++ helper_functions/get_fundamental_indicators_test.py
@@ -11,23 +11,23 @@ from datetime import date, datetime, timedelta
 class TestCompositeStock(unittest.TestCase):
     def test_is_composite_stock_true(self):
         stock_info = {'symbol': '^IXIC'}
-        self.assertTrue(get_fundamental_indicators.is_composite_stock(stock_info))
+        self.assertTrue(get_fundamental_indicators.is_composite_stock(stock_info, "^IXIC"))
 
     def test_is_goog_false(self):
         stock_info = {'symbol': 'GOOGL'}
-        self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info))
+        self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info, 'GOOGL'))
         
     def test_is_short_composite_true(self):
         stock_info = {'symbol': 'DJI'}
-        self.assertTrue(get_fundamental_indicators.is_composite_stock(stock_info))
+        self.assertTrue(get_fundamental_indicators.is_composite_stock(stock_info, 'DJI'))
 
     def test_is_blank_stock_false(self):
         stock_info = {'symbol': ''}
-        self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info))
+        self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info, ""))
 
     def test_is_empty_false(self):
             stock_info = {}
-            self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info))
+            self.assertFalse(get_fundamental_indicators.is_composite_stock(stock_info, None))
 
 ## get_all_symbols
 class TestgetAllSymbols(unittest.TestCase):
@@ -50,32 +50,33 @@ class TestgetAllSymbolObject(unittest.TestCase):
 class TestgetSymbolObject(unittest.TestCase):
     def test_retrieved_symbol(self):
         value = get_fundamental_indicators.get_symbol_object("GOOG", "NASDAQ")
-        test = value['data'].info['symbol']
+        test = value['symbol']
         self.assertEqual(test, 'GOOG') # there are always atleast 10k symbols in the indexes
 
 ## get_good_stock_data
 stock_keys = ['shortName', 'industry', 'symbol', 'exchange', 'currentPrice', 'marketCap', 'heldPercentInstitutions', 'dividendYield', 'exDividendDate', 'forwardEps', 'trailingEps', 'forwardPE', 'trailingPE', 'priceToBook', 'profitMargins', 'totalRevenue', 'grossProfits', 'returnOnAssets', 'returnOnEquity', 'debtToEquity', 'sharesOutstanding', 'floatShares', 'nextEarningsDate', '4Quarters_increasing_revenue', '4Quarters_increasing_profit', '4years_increasing_revenue', '4years_increasing_profit', 'pct_chg_5y', 'pct_chg_1y', 'pct_chg_6mo', 'pct_chg_3mo', 'pct_chg_1mo', 'pct_chg_1wk', 'fundamentals_url', 'daily_chart', '5mo_chart']
 class Testget_good_stock_data(unittest.TestCase):
     def test_retrieved_good_stock(self):
-        stock = get_fundamental_indicators.get_symbol_object("CSX", "NASDAQ")
-        value = get_fundamental_indicators.get_good_stock_data(stock)
+        self.maxDiff = None
+        stock = get_fundamental_indicators.get_symbol_object("ADI", "NASDAQ")
+        value = get_fundamental_indicators.get_good_stock_data(stock, get_any_stock=True)
         self.assertEqual(list(value.keys()), stock_keys)
 
 ## write_symbol_to_csv
 ## correct keys for the stock object
 csv_cleaned_keys = ['shortName', 'industry', 'symbol', 'exchange', 'currentPrice', 'heldPercentInstitutions', 'dividendYield', 'exDividendDate', 'forwardPE', 'trailingPE', 'priceToBook', 'nextEarningsDate', '4Quarters_increasing_revenue', '4Quarters_increasing_profit', '4years_increasing_revenue', '4years_increasing_profit', 'pct_chg_5y', 'pct_chg_1y', 'pct_chg_6mo', 'pct_chg_3mo', 'pct_chg_1mo', 'pct_chg_1wk', 'fundamentals_url', 'daily_chart', '5mo_chart']
 class Testget_write_symbol_to_csv(unittest.TestCase):
-    ### good stock - csx
+    ### good stock - ADI
     def test_csv_retrieved_good_stock(self):
-        value = get_fundamental_indicators.write_symbol_to_csv("CSX", "NASDAQ", cache=False)
+        value = get_fundamental_indicators.write_symbol_to_csv("ADI", "NASDAQ", cache=False)
         self.assertEqual(list(value.keys()), csv_cleaned_keys)
         ### CSV exists
-        file_path = general.resultsPath("CSX_stock_data.csv")
+        file_path = general.resultsPath("ADI_stock_data.csv")
         assert os.path.exists(file_path)
         ### cache exists
-        value = get_fundamental_indicators.write_symbol_to_csv("CSX", "NASDAQ", cache=True)
+        value = get_fundamental_indicators.write_symbol_to_csv("ADI", "NASDAQ", cache=True)
         self.assertEqual(list(value.keys()), csv_cleaned_keys)
-        file_path = general.dataPath("CSX_stock_data"+str(date.today())+".pkl")
+        file_path = general.dataPath("ADI_stock_data"+str(date.today())+".pkl")
         assert os.path.exists(file_path)
         ### correct keys
         self.assertEqual(list(value.keys()), csv_cleaned_keys)
@@ -92,9 +93,9 @@ class Testget_write_symbol_to_csv(unittest.TestCase):
 
     ### get_any_stock - good stock
     def test_csv_retrieve_good_stock_via_any_stock_option(self):
-        value = get_fundamental_indicators.write_symbol_to_csv("CSX", "NASDAQ", cache=True, get_any_stock=True)
+        value = get_fundamental_indicators.write_symbol_to_csv("ADI", "NASDAQ", cache=True, get_any_stock=True)
         self.assertEqual(list(value.keys()), csv_cleaned_keys)
-        value = get_fundamental_indicators.write_symbol_to_csv("CSX", "NASDAQ", cache=False, get_any_stock=True)
+        value = get_fundamental_indicators.write_symbol_to_csv("ADI", "NASDAQ", cache=False, get_any_stock=True)
         self.assertEqual(list(value.keys()), csv_cleaned_keys)
 
     ### get_any_stock - index
@@ -108,7 +109,7 @@ class Testget_write_symbol_to_csv(unittest.TestCase):
 class Testget_write_symbols_to_csv(unittest.TestCase):
     ### good stocks
     def test_all_csv_retrieved_good_stocks(self):
-        added=[{'symbol': "GOOG", 'exchange': "NASDAQ"},{'symbol': "CSX", 'exchange': "NASDAQ"},{'symbol': "CNI", 'exchange': "NYSE"},{'symbol': "CAT", 'exchange': "NASDAQ"},{'symbol': "V", 'exchange': "NYSE"},{'symbol': "^IXIC", 'exchange': "NASDAQ"},{'symbol': "^NYA", 'exchange': "NYSE"}]
+        added=[{'symbol': "GOOG", 'exchange': "NASDAQ"},{'symbol': "ADI", 'exchange': "NASDAQ"},{'symbol': "CNI", 'exchange': "NYSE"},{'symbol': "CAT", 'exchange': "NASDAQ"},{'symbol': "V", 'exchange': "NYSE"},{'symbol': "^IXIC", 'exchange': "NASDAQ"},{'symbol': "^NYA", 'exchange': "NYSE"}]
         blacklist=[{'symbol': "GOOG", 'exchange': "NASDAQ"}, {'symbol': "DUK", 'exchange': "NYSE"}]
         value = get_fundamental_indicators.write_symbols_to_csv(added_tickers=added, blacklisted_tickers=blacklist,do_all_tickers=False)
         self.assertEqual(len(value), 6) #7added-1blacklist=6
@@ -122,3 +123,6 @@ class Testget_write_symbols_to_csv(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+    #suite = unittest.TestSuite()
+    #suite.addTest(Testget_good_stock_data("test_retrieved_good_stock"))
+    #unittest.TextTestRunner().run(suite)
