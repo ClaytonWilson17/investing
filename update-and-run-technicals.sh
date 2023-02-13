af5bcc444028a08f370128b3cf6057a1c6a30233
Simon Owens <sowens@vailsys.com>
2023-02-13 14:10:27 -0600

    use stored data in the technicals scripts because fundamentals are broken

--- update-and-run-technicals.sh
+++ update-and-run-technicals.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 git pull
 # inside python container
-docker container run -w="/src/" -v /media/apps/investing/:/src/ -t python:3.10 /bin/bash -c "pip3 install -r requirements.txt; python3 main.py --technical_only"
+docker container run -w="/src/" -v /media/apps/investing/:/src/ -t python:3.10 /bin/bash -c "pip3 install -r requirements.txt; python3 main.py --technical_only --stored_data"
